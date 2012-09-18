805ed9908c9443c122f275d5b13e7132dd522f8d
James Chen <jameschen@actions-micro.com>
2012-09-18 11:19:35 +0800

    add default client

--- src/com/actionsmicro/pigeon/Pigeon.java
+++ src/com/actionsmicro/pigeon/Pigeon.java
@@ -2,10 +2,14 @@ package com.actionsmicro.pigeon;
 
 public class Pigeon {
 	public static Client createPigeonClient(final String version, final String serverAddress, int portNumber) {
-		if (version.equals("2")) {
-			return new ClientV2(serverAddress, portNumber);
+		if (version != null) {
+			if (version.equals("2")) {
+				return new ClientV2(serverAddress, portNumber);
+			} else {
+				return new Client(serverAddress, portNumber);				
+			}
 		} else {
-			return new Client(serverAddress, portNumber);				
+			return new Client(serverAddress, portNumber);							
 		}
 	}
 }
