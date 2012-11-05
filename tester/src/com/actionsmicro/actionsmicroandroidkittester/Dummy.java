398062b0660ac9249aaa45162b089a1c1f4d6e46
James Chen <jameschen@actions-micro.com>
2012-11-05 18:35:21 +0800

    add dummy class

---
+++ tester/src/com/actionsmicro/actionsmicroandroidkittester/Dummy.java
@@ -0,0 +1,21 @@
+package com.actionsmicro.actionsmicroandroidkittester;
+
+import junit.framework.TestCase;
+
+public class Dummy extends TestCase {
+
+	protected static void setUpBeforeClass() throws Exception {
+	}
+
+	protected static void tearDownAfterClass() throws Exception {
+	}
+
+	protected void setUp() throws Exception {
+		super.setUp();
+	}
+
+	protected void tearDown() throws Exception {
+		super.tearDown();
+	}
+
+}
