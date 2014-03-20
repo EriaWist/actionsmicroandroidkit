4aa87bb72a3d27a41a5bf26a6865a5c2fdb4c5f2
James Chen <jameschen@actions-micro.com>
2014-03-20 09:58:08 +0800

    MJpegServer: Move defer sending from client to SimpleMotionJpegHttpServer

--- src/com/actionsmicro/web/SimpleMotionJpegHttpServer.java
+++ src/com/actionsmicro/web/SimpleMotionJpegHttpServer.java
@@ -28,8 +28,9 @@ public class SimpleMotionJpegHttpServer {
 	private ServerSocket server;
 	private Context context;
     
-	public SimpleMotionJpegHttpServer(Context context, final int port) {
+	public SimpleMotionJpegHttpServer(Context context, final int port, OnConnectionListener listener) {
 		this.context = context;
+		this.onConnectionListener = listener;
 		try {
 			server = new ServerSocket(port);
 			server.setReuseAddress(true);
@@ -51,28 +52,41 @@ public class SimpleMotionJpegHttpServer {
 		
 		
 	}
-	private synchronized void createServerAndWaitForConnection(int port) {
+	public interface OnConnectionListener {
+
+		void onClientConnected(
+				SimpleMotionJpegHttpServer simpleMotionJpegHttpServer);
+		
+	}
+	private OnConnectionListener onConnectionListener;
+	private void createServerAndWaitForConnection(int port) {
 		try {
 			while (!shouldStop) {
 				try {
 					Socket newSocket = server.accept();
 					if (newSocket != null) {
-						cleanUpConnection();
-						socket = newSocket;
-						Log.d(TAG, "New connection to :" + socket.getInetAddress());
-						outputStream = new DataOutputStream(socket.getOutputStream());
-						outputStream.write(("HTTP/1.0 200 OK\r\n" +
-								"Server: EZCastStreamer\r\n" +
-								"Connection: close\r\n" +
-								"Max-Age: 0\r\n" +
-								"Expires: 0\r\n" +
-								"Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0\r\n" +
-								"Pragma: no-cache\r\n" + 
-								"Content-Type: multipart/x-mixed-replace; " +
-								"boundary=" + boundary + "\r\n" +
-								"\r\n" +
-								//						"--" + boundary + "\r\n" +
-								"").getBytes());
+						synchronized (SimpleMotionJpegHttpServer.this) {
+							cleanUpConnection();
+							socket = newSocket;
+							Log.d(TAG, "New connection to :" + socket.getInetAddress());
+							outputStream = new DataOutputStream(socket.getOutputStream());
+							outputStream.write(("HTTP/1.0 200 OK\r\n" +
+									"Server: EZCastStreamer\r\n" +
+									"Connection: close\r\n" +
+									"Max-Age: 0\r\n" +
+									"Expires: 0\r\n" +
+									"Cache-Control: no-store, no-cache, must-revalidate, pre-check=0, post-check=0, max-age=0\r\n" +
+									"Pragma: no-cache\r\n" + 
+									"Content-Type: multipart/x-mixed-replace; " +
+									"boundary=" + boundary + "\r\n" +
+									"\r\n" +
+									//						"--" + boundary + "\r\n" +
+									"").getBytes());
+							if (onConnectionListener != null) {
+								onConnectionListener.onClientConnected(SimpleMotionJpegHttpServer.this);
+							}
+							sendJepgStreamImp();
+						}
 					}
 				} catch (SocketTimeoutException e) {
 
@@ -86,17 +100,25 @@ public class SimpleMotionJpegHttpServer {
 		}
 	}
 	static ByteArrayOutputStream clone = new ByteArrayOutputStream(500*1024);
-	public void sendJpegStream(InputStream jpegStream, long size) {
-		if (isConnected()) {
-			Log.d(TAG, "sendJpegStream to :" + socket.getInetAddress());
-			
+	public synchronized void sendJpegStream(InputStream jpegStream, long size) {
+		Log.d(TAG, "clone start");			
+		clone.reset();
+		try {
+			Utils.dump(jpegStream, clone);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			clone.reset();
+		}
+		Log.d(TAG, "clone end");			
+		sendJepgStreamImp();
+	}
+	private synchronized void sendJepgStreamImp() {
+		if (isConnected() && clone.size() > 0) {
+			Log.d(TAG, "sendJpegStream to :" + socket.getInetAddress());			
 			try {
-				clone.reset();
-				Utils.dump(jpegStream, clone);
-				
 				for (int i =0; i< 1; i++) {
 					outputStream.write(("Content-type: image/jpeg\r\n" +
-							"Content-Length: " + size + "\r\n" +
+							"Content-Length: " + clone.size() + "\r\n" +
 //							"X-Timestamp:" + new Date().getTime() + "\r\n" +
 							"\r\n" +
 							"").getBytes());
@@ -120,9 +142,11 @@ public class SimpleMotionJpegHttpServer {
 				Log.e(TAG, e.getLocalizedMessage());				
 				cleanUpConnection();
 			}
+		} else {
+			Log.d(TAG, "No connection or clone is empty. close size:"+clone.size());
 		}
 	}
-	public boolean isConnected() {
+	public synchronized boolean isConnected() {
 		return outputStream != null;
 	}
 	public void cleanup() {
@@ -179,5 +203,11 @@ public class SimpleMotionJpegHttpServer {
 			}
 		}
 		return null;
+	}
+	public OnConnectionListener getOnConnectionListener() {
+		return onConnectionListener;
+	}
+	public void setOnConnectionListener(OnConnectionListener onConnectionListener) {
+		this.onConnectionListener = onConnectionListener;
 	}
 }
