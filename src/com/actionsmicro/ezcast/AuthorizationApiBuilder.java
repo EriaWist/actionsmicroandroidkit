a10ebfdf1a16f045d46cfed40af4caa6116e33ed
James Chen <jameschen@actions-micro.com>
2014-04-22 10:10:09 +0800

    remove redundant code

--- src/com/actionsmicro/ezcast/AuthorizationApiBuilder.java
+++ src/com/actionsmicro/ezcast/AuthorizationApiBuilder.java
@@ -3,8 +3,6 @@ package com.actionsmicro.ezcast;
 import android.content.Context;
 
 import com.actionsmicro.ezcast.AuthorizationApi.AuthorizationListener;
-import com.actionsmicro.ezcast.imp.ezdisplay.PigeonAuthorizationApi;
-import com.actionsmicro.ezcast.imp.ezdisplay.PigeonDeviceInfo;
 
 public class AuthorizationApiBuilder extends ApiBuilder<AuthorizationApi> {
 	private AuthorizationListener authorizationListener;
@@ -17,9 +15,6 @@ public class AuthorizationApiBuilder extends ApiBuilder<AuthorizationApi> {
 	}
 	@Override
 	public AuthorizationApi build() {
-		if (getDevice() instanceof PigeonDeviceInfo) {
-			return new PigeonAuthorizationApi(this);
-		}		
 		return getDevice().createAuthorizationApi(this);
 	}
 	public AuthorizationListener getAuthorizationListener() {
