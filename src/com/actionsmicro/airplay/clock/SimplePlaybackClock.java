dbb55a8509b020adb2e97096c4492e526b1f461c
James Chen <jameschen@actions-micro.com>
2014-05-09 16:33:55 +0800

    AirPlay: Add playback clock

---
+++ src/com/actionsmicro/airplay/clock/SimplePlaybackClock.java
@@ -0,0 +1,56 @@
+package com.actionsmicro.airplay.clock;
+
+import com.actionsmicro.utils.Log;
+
+public class SimplePlaybackClock {
+//	private static final long LATENCY_TOLERANCE = 200; //ms
+	private static final long EARLY_TOLERANCE = 10; //ms
+	private long timestampBase;
+	private long systemTimeBase;
+	private String debugTag;
+	private long latencyTolerance;
+	private static final String TAG = "SimplePlaybackClock";
+	private static final boolean DEBUG_LOG = false;
+	
+	public SimplePlaybackClock(long presentationTime, long latencyTolerance, String debugTag) {
+		this.debugTag = debugTag;
+		this.latencyTolerance = latencyTolerance;
+		sync(presentationTime);
+	}
+	public void sync(long presentationTime) {
+		timestampBase = presentationTime;
+		systemTimeBase = System.currentTimeMillis();
+	}
+	public boolean waitUntilTime(long presentationTime) {
+		presentationTime = presentationTime - timestampBase;
+		if (presentationTime < now()) {
+			if ((now() - presentationTime) > latencyTolerance) {
+				debugLog("presentationTime:"+presentationTime+" is too late for "+(now() - presentationTime)+"ms");
+				return false;
+			}
+		} else if ((presentationTime - now()) > EARLY_TOLERANCE) {
+			while (presentationTime > now() && !Thread.currentThread().isInterrupted()) {
+				try {
+					long wait = presentationTime - now();
+					debugLog("presentationTime:"+presentationTime+" is too early for "+wait+"ms. let's wait");
+					if (wait <= 0) {
+						break;
+					}
+					Thread.sleep(wait);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+	private long now() {
+		return System.currentTimeMillis() - systemTimeBase;
+	}
+	private void debugLog(String msg) {
+		if (DEBUG_LOG) {
+			Log.d(TAG+"."+debugTag, msg);
+		}
+	}
+}
