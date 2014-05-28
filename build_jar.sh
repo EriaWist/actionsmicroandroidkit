6a86fb978be735dbbe11e97b17e4ccb6da67de5c
James Chen <jameschen@actions-micro.com>
2014-05-28 16:00:31 +0800

    Change build settings Falcon: Fix text encoding of source code

---
+++ build_jar.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+echo android project for ant
+android update lib-project -p .
+
+echo update project appcompat
+android update lib-project -p ./library_projects/appcompat
+
+echo update project mediarouter
+android update lib-project -p ./library_projects/mediarouter
+
+echo update project google-play-services_lib
+android update lib-project -p ./library_projects/google-play-services_lib
+
+echo build jar file
+ant -buildfile build.xml release
+
+echo rename jar file with version string
+mv bin/proguard/obfuscated.jar bin/proguard/$1_$2.jar
+
+echo generate Javadoc
+ant -buildfile javadoc.xml javadoc
+
