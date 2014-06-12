68ef305f94a28a47510fa5390f7d1c7c9be8b5d1
James Chen <jameschen@actions-micro.com>
2014-06-12 15:29:50 +0800

    SDK: Fix build script

--- build_jar.sh
+++ build_jar.sh
@@ -13,7 +13,7 @@ echo update project google-play-services_lib
 android update lib-project -p ./library_projects/google-play-services_lib
 
 echo build jar file
-ant -buildfile build.xml release
+ant -buildfile build.xml clean release
 
 echo rename jar file with version string
 mv bin/proguard/obfuscated.jar bin/proguard/$1_$2.jar
