2aae2feee6e154de927aa475cd18177f276d8a27
James Chen <jameschen@actions-micro.com>
2014-10-13 17:49:56 +0800

    Refine project dependencies

--- build_jar.sh
+++ build_jar.sh
@@ -3,20 +3,23 @@
 echo android project for ant
 android update lib-project -p .
 
-echo update project appcompat
-android update lib-project -p ./library_projects/appcompat
+# echo update project appcompat
+# android update lib-project -p ./library_projects/appcompat
 
-echo update project mediarouter
-android update lib-project -p ./library_projects/mediarouter
+# echo update project mediarouter
+# android update lib-project -p ./library_projects/mediarouter
 
-echo update project google-play-services_lib
-android update lib-project -p ./library_projects/google-play-services_lib
+# echo update project google-play-services_lib
+# android update lib-project -p ./library_projects/google-play-services_lib
 
 echo build jar file
 ant -buildfile build.xml clean release
 
 echo rename jar file with version string
-mv bin/proguard/obfuscated.jar bin/proguard/$1_$2.jar
+if [ ! -d "sdk_template/ezcast-sdk_lib/libs" ]; then
+	mkdir sdk_template/ezcast-sdk_lib/libs
+fi
+mv bin/proguard/obfuscated.jar sdk_template/ezcast-sdk_lib/libs/$1_$2.jar
 
 export SDK_VERSION_STRING=${BUILD_VERSION_STRING}
 export BUILD_YEAR=`date +'%Y'`
