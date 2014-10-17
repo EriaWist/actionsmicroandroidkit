6fd9f7f3075db382f09f686e1c21420ac351ea0f
James Chen <jameschen@actions-micro.com>
2014-10-17 17:47:34 +0800

    UnitTest: Refactor some share local fields as instance fields

--- src/com/actionsmicro/analytics/unittest/UsageArchiverTest.java
+++ src/com/actionsmicro/analytics/unittest/UsageArchiverTest.java
@@ -13,25 +13,24 @@ import com.actionsmicro.analytics.usage.WebVideoUsage;
 import com.google.gson.Gson;
 
 public class UsageArchiverTest extends TestCase {
-	Mockery context = new Mockery();
-	Gson gson = new Gson();
+	final Mockery context = new Mockery();
+	final Gson gson = new Gson();
+	final String packageName = "com.mock.app";
+	final MockContext mockAndroidContext = new MockContext() {
+		@Override
+		public String getPackageName() {
+			return packageName;
+		}
+	};
 	public void testWebVideoUsage() {
-		final String packageName = "com.mock.app";
 		final String mockUrl = "http://host.com:3030/path?q=1&u=2#frag";
-		MockContext mockContext = new MockContext() {
-			@Override
-			public String getPackageName() {
-				return packageName;
-			}
-		};
 		final Tracker tracker = context.mock(Tracker.class);
-		final WebVideoUsage usage = new WebVideoUsage(tracker, mockContext, mockUrl);
+		final WebVideoUsage usage = new WebVideoUsage(tracker, mockAndroidContext, mockUrl);
 		context.checking(new Expectations() {{
 			oneOf (tracker).log(with(same(usage)));
 		}});
 		usage.begin();
 		usage.commit();
-		
 		context.assertIsSatisfied();
 		try {
 			JSONObject jsonObject = new JSONObject(gson.toJson(usage));
