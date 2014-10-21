6b8a67b6710abd136252a2fb2893fb6a4786612c
James Chen <jameschen@actions-micro.com>
2014-10-21 17:37:09 +0800

    remove redundant log

--- src/com/actionsmicro/analytics/unittest/AuthenticationHashTest.java
+++ src/com/actionsmicro/analytics/unittest/AuthenticationHashTest.java
@@ -1,14 +1,12 @@
 package com.actionsmicro.analytics.unittest;
 
 import junit.framework.TestCase;
-import android.util.Log;
 
 import com.actionsmicro.analytics.tracker.HashUtils;
 
 public class AuthenticationHashTest extends TestCase {
 	public void testHash() {
     	try {
-			Log.d("AuthenticationHashTest", HashUtils.SHA1("secret_key\t1413789111\t/cloud/sdk/api"));
 			assertEquals("38e5efa1992888c6de32111f425772bf4896364b", HashUtils.SHA1("secret_key\t1413789111\t/cloud/sdk/api"));
 		} catch (Throwable t) {
 			t.printStackTrace();
