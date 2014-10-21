5d0654f64316a4566ad5512437ac3a16dcbea81b
James Chen <jameschen@actions-micro.com>
2014-10-21 11:07:38 +0800

    move uploader

---
+++ src/com/actionsmicro/analytics/tracker/ActionsUploader.java
@@ -0,0 +1,10 @@
+package com.actionsmicro.analytics.tracker;
+
+public class ActionsUploader {
+	public ActionsUploader(String appKey, String appSecret) {
+		
+	}
+	public void uploadLog() {
+		
+	}
+}
