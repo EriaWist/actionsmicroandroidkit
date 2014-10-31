f49d5b6c6f8ccae630874e072b274520ab63a76c
James Chen <jameschen@actions-micro.com>
2014-10-31 10:51:01 +0800

    Refine LogTracker

--- src/com/actionsmicro/analytics/tracker/LogTracker.java
+++ src/com/actionsmicro/analytics/tracker/LogTracker.java
@@ -2,6 +2,9 @@ package com.actionsmicro.analytics.tracker;
 
 import java.util.Map;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import com.actionsmicro.analytics.AppInfo;
 import com.actionsmicro.analytics.Tracker;
 import com.actionsmicro.analytics.Usage;
@@ -14,17 +17,35 @@ public class LogTracker implements Tracker {
 	
 	@Override
 	public void log(Usage usage) {
-		Log.d("LogTracker", gson.toJson(usage));
+		logJson(gson.toJson(usage));
+	}
+
+	private void logJson(String json) {
+		JSONObject jsonObject = null;
+		try {
+			jsonObject = new JSONObject(json);
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+		if (jsonObject != null && jsonObject.has("type")) {
+			try {
+				Log.d("LogTracker", "Record type:"+jsonObject.getString("type") + "\n" + json);
+			} catch (JSONException e) {
+				e.printStackTrace();
+			}
+		} else {
+			Log.d("LogTracker", json);
+		}
 	}
 
 	@Override
 	public void log(AppInfo appInfo) {
-		Log.d("LogTracker", gson.toJson(appInfo));
+		logJson(gson.toJson(appInfo));
 	}
 
 	@Override
 	public void log(Map<String, Object> map) {
-		Log.d("LogTracker", gson.toJson(map));		
+		logJson(gson.toJson(map));		
 	}
 
 }
