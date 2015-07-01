2edc8189faca19618f64e88db883a688656ecf19
fredchen <fredchen@actions-micro.com>
2015-07-01 15:18:02 +0800

    Fix typo

--- src/main/java/com/actionsmicro/androidkit/ezcast/imp/ezdisplay/PigeonAudioApi.java
+++ src/main/java/com/actionsmicro/androidkit/ezcast/imp/ezdisplay/PigeonAudioApi.java
@@ -16,7 +16,7 @@ public class PigeonAudioApi extends PigeonApi implements AudioApi {
      * Send audio data to the device.
      * @param inputStream InputStream which wraps the PCM Audio data.
      * @throws Exception
-     * @since 2.0
+     * @since 2.3
      */
     @Override
     public void sendAudioEncodedData(InputStream inputStream) throws IllegalArgumentException, IOException {
