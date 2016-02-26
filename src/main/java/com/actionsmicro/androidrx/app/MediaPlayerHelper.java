e7648993c825ad15e47eda366e778e1785ccace6
fredchen <fredchen@actions-micro.com>
2016-02-26 17:07:58 +0800

    Add VLC to MediaPlayerHelper for rx, refine EZScreenHelper for floating window 1.Extract updateTransform 2.Increase payload buffer size to fix ArrayOutOfBoundException 3.Replace videoview with surfaceview 4.Remove mediacontrol since it need activity instead of context 5.Re-enqueue csd data after hide/show (still have issue for some device:ex nesus 9)

--- src/main/java/com/actionsmicro/androidrx/app/MediaPlayerHelper.java
+++ src/main/java/com/actionsmicro/androidrx/app/MediaPlayerHelper.java
@@ -1,35 +1,45 @@
 package com.actionsmicro.androidrx.app;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.media.MediaPlayer;
 import android.media.MediaPlayer.OnCompletionListener;
 import android.media.MediaPlayer.OnErrorListener;
 import android.media.MediaPlayer.OnInfoListener;
 import android.media.MediaPlayer.OnPreparedListener;
-import android.os.Build;
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.DisplayMetrics;
 import android.view.Gravity;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.FrameLayout;
-import android.widget.MediaController;
+import android.widget.Toast;
 import android.widget.VideoView;
 
 import com.actionsmicro.utils.Log;
 
-public class MediaPlayerHelper {
+import org.videolan.libvlc.IVLCVout;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+
+import java.util.ArrayList;
+
+public class MediaPlayerHelper implements IVLCVout.Callback, LibVLC.HardwareAccelerationError{
 
 	private static final String TAG = "MediaPlayerHelper";
 	private ViewGroup container;
 	private PlayerListener playerListener;
 	private boolean stopped;
-	private MediaController mediaController;
-	protected int bufferPercentage;
+//	private MediaController mediaController;
 	protected int duration = -1;
 	private VideoView videoView;
 	protected MediaPlayer mediaPlayer;
+	private Context mCtx;
+
 	public interface PlayerListener {
 		public void onDurationChange(int seconds);
 		public void onLoadStart();
@@ -44,16 +54,17 @@ public class MediaPlayerHelper {
 		public void onLoadedMetadata();
 	}
 	public MediaPlayerHelper(final Context context, final ViewGroup container, final PlayerListener playerListener) {
+		mCtx = context;
 		this.container = container;
 		this.playerListener = playerListener;
-		
 		runOnUiThread(new Runnable() {
 
 			@Override
 			public void run() {
-				initVideoView(context, container, playerListener);				
+				initSurfaceView(context,container,playerListener);
+//				initVideoView(context, container, playerListener);
 			}
-			
+
 		});
 	}
 	private Handler mainHandler = new Handler(Looper.getMainLooper()) ;
@@ -67,6 +78,15 @@ public class MediaPlayerHelper {
 				}
 			}
 		}
+	}
+
+	private void initSurfaceView(final Context context,
+							   final ViewGroup container, final PlayerListener playerListener) {
+		mSurface = new SurfaceView(context);
+		FrameLayout.LayoutParams layoutParams = new FrameLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT, Gravity.CENTER);
+		mSurface.setLayoutParams(layoutParams);
+		container.addView(mSurface);
+		createMediaPlayerifNeeded();
 	}
 	private void initVideoView(final Context context,
 			final ViewGroup container, final PlayerListener playerListener) {
@@ -149,16 +169,16 @@ public class MediaPlayerHelper {
 			}
 			
 		});
-		mediaController = new MediaController(context);
-		videoView.setMediaController(mediaController);
+//		mediaController = new MediaController(context);
+//		videoView.setMediaController(mediaController);
 	}
 
-	@SuppressLint("NewApi")
-	private void setOnInfoListener(final PlayerListener playerListener) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-			videoView.setOnInfoListener(onInfoListener);
-		}
-	}
+//	@SuppressLint("NewApi")
+//	private void setOnInfoListener(final PlayerListener playerListener) {
+//		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
+//			videoView.setOnInfoListener(onInfoListener);
+//		}
+//	}
 	private OnInfoListener onInfoListener = new OnInfoListener() {
 
 		@Override
@@ -222,20 +242,9 @@ public class MediaPlayerHelper {
 		});
 	}
 	private void loadImp(String url) {
-		if (videoView != null) {
-			try {
-				videoView.setVideoPath(url);
-				if (playerListener != null) {
-					playerListener.onLoadStart();
-				}
-				showControl(0);
-
-			} catch (Throwable e) {
-				if (playerListener != null) {
-					playerListener.onError(4);
-				}
-				e.printStackTrace();
-			}
+		setMediaPath(url);
+		if (playerListener != null) {
+			playerListener.onLoadStart();
 		}
 	}
 
@@ -247,47 +256,54 @@ public class MediaPlayerHelper {
 			public void run() {
 				playImp(startpos);
 			}
-			
+
 		});
 	}
 	private void playImp(int startpos) {
-		if (videoView != null) {
-			try {
-				if (startpos != 0) {
-					videoView.seekTo(startpos);
-				}
-				videoView.start();
-				showControl();
-				if (playerListener != null) {
-					playerListener.onPlay();
-				}
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
+		playVideoAtPath();
+		if (playerListener != null) {
+			playerListener.onPlay();
+		}
+
+//		if (videoView != null) {
+//			try {
+//				if (startpos != 0) {
+//					videoView.seekTo(startpos);
+//				}
+//				videoView.start();
+////				showControl();
+//				if (playerListener != null) {
+//					playerListener.onPlay();
+//				}
+//			} catch (Throwable e) {
+//				e.printStackTrace();
+//			}
+//		}
+	}
+
+	private void setMediaPath(String media) {
+		Media m;
+		if (media.contains("http")) {
+			m = new Media(libvlc, Uri.parse(media));
+		} else {
+			m = new Media(libvlc, media);
+		}
+
+		if (media.contains("mov") || media.contains("mts")) {
+			m.setHWDecoderEnabled(false, false);
+		}
+
+		mMediaPlayer.setMedia(m);
+	}
+
+	private void playVideoAtPath() {
+		try {
+			mMediaPlayer.play();
+		} catch (Exception e) {
+			Log.e(TAG, "Error creating player!", e);
 		}
 	}
 
-	private void showControl(final int timeout) {
-		if (videoView != null) {
-			videoView.post(new Runnable() {
-
-				@Override
-				public void run() {
-					try {
-						if (mediaController != null) {
-							mediaController.show(timeout);
-						}
-					} catch (Throwable e) {
-						e.printStackTrace();
-					}
-				}
-
-			});
-		}
-	}
-	private void showControl() {
-		showControl(3000);
-	}
 	public void stop() {
 		Log.v(TAG, "stop:");
 		runOnUiThread(new Runnable() {
@@ -300,19 +316,11 @@ public class MediaPlayerHelper {
 		});
 	}
 	private void stopImp() {
-		if (videoView != null) {
-			try {
-				if (videoView.isPlaying()) {
-					videoView.stopPlayback();
-				}
-				container.removeView(videoView);					
-				videoView = null;
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
+		if(mMediaPlayer!=null) {
+			mMediaPlayer.stop();
+			container.removeView(mSurface);
+			releasePlayer();
 		}
-		stopped = true;
-		duration = -1;
 	}
 
 	public void pause() {
@@ -328,16 +336,8 @@ public class MediaPlayerHelper {
 
 	}
 	private void pauseImp() {
-		if (videoView != null) {
-			try {
-				videoView.pause();
-				showControl(0);
-				if (playerListener != null) {
-					playerListener.onPaused();
-				}
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
+		if (mMediaPlayer != null) {
+			mMediaPlayer.pause();
 		}
 	}
 
@@ -353,16 +353,8 @@ public class MediaPlayerHelper {
 		});
 	}
 	private void resumeImp() {
-		if (videoView != null) {
-			try {
-				videoView.start();
-				showControl();
-				if (playerListener != null) {
-					playerListener.onPlaying();
-				}
-			} catch (Throwable e) {
-				e.printStackTrace();
-			}
+		if (mMediaPlayer != null) {
+			mMediaPlayer.play();
 		}
 	}
 
@@ -389,18 +381,206 @@ public class MediaPlayerHelper {
 		});
 	}
 	private void seekImp(int msec) {
-		if (videoView != null) {
-			try {
-				if (videoView.isPlaying()) {
-					showControl();
-				} else {
-					showControl(0);					
-				}
-				videoView.seekTo(msec);
-			} catch (Throwable e) {
-				e.printStackTrace();
+		if(null != mMediaPlayer) {
+			long leng = mMediaPlayer.getLength();
+			if (0l == leng) {
+				mMediaPlayer.setTime(msec);
+			} else {
+				mMediaPlayer.setPosition(msec * 1f / mMediaPlayer.getLength());
 			}
 		}
 	}
 
+	private SurfaceHolder holder;
+	private LibVLC libvlc;
+	private org.videolan.libvlc.MediaPlayer mMediaPlayer = null;
+	private int mVideoWidth;
+	private int mVideoHeight;
+	private org.videolan.libvlc.MediaPlayer.EventListener mPlayerListener = new PlayerEventListener();
+	private SurfaceView mSurface;
+
+	private class PlayerEventListener implements org.videolan.libvlc.MediaPlayer.EventListener {
+		@Override
+		public void onEvent(org.videolan.libvlc.MediaPlayer.Event event) {
+			Log.d(TAG,"event = " + event.type);
+			switch(event.type) {
+				case org.videolan.libvlc.MediaPlayer.Event.Opening:
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.EndReached:
+					playerListener.onEnded();
+//					mediaPlayerStateListener.mediaPlayerDidStop(DemoMediaPlayerApi.this, MediaPlayerApi.Cause.REMOTE);
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.TimeChanged:
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.SeekableChanged:
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.PositionChanged:
+					playerListener.onTimeUpdate((int) (mMediaPlayer.getTime()/1000));
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.Playing:
+					playerListener.onPlaying();
+					playerListener.onDurationChange((int) (mMediaPlayer.getLength()/1000));
+//					setDuration((int) (mMediaPlayer.getLength()/1000));
+//					mState = MediaPlayerApi.State.PLAYING;
+//					mediaPlayerStateListener.mediaPlayerDidStart(DemoMediaPlayerApi.this);
+//					mediaPlayerStateListener.mediaPlayerDurationIsReady(DemoMediaPlayerApi.this,mMediaPlayer.getLength()/1000);
+					break;
+				case org.videolan.libvlc.MediaPlayer.Event.Paused:
+					playerListener.onPaused();
+//				}
+				case org.videolan.libvlc.MediaPlayer.Event.Stopped:
+				default:
+					break;
+			}
+		}
+	}
+
+	private void initVLCPlayer() {
+		if (null == libvlc) {
+			ArrayList<String> options = new ArrayList<String>();
+
+			//options.add("--subsdec-encoding <encoding>");
+			options.add("--aout=opensles");
+			options.add("--audio-time-stretch"); // time stretching
+			options.add("-vvv"); // verbosity
+
+			libvlc = new LibVLC(options);
+
+			libvlc.setOnHardwareAccelerationError(this);
+		}
+
+		if(mMediaPlayer == null) {
+			mMediaPlayer = new org.videolan.libvlc.MediaPlayer(libvlc);
+			// Create media player
+			mMediaPlayer.setEventListener(mPlayerListener);
+		}
+	}
+
+	private void createMediaPlayerifNeeded() {
+		initVLCPlayer();
+
+		if (mSurface != null) {
+			if (!mMediaPlayer.getVLCVout().areViewsAttached()) {
+				holder = mSurface.getHolder();
+				holder.setKeepScreenOn(true);
+				holder.addCallback(new SurfaceHolder.Callback2() {
+					@Override
+					public void surfaceRedrawNeeded(SurfaceHolder holder) {
+						final IVLCVout vout = mMediaPlayer.getVLCVout();
+						if(!vout.areViewsAttached()) {
+							vout.setVideoView(mSurface);
+							vout.addCallback(MediaPlayerHelper.this);
+							vout.attachViews();
+						}
+					}
+
+					@Override
+					public void surfaceCreated(SurfaceHolder holder) {
+					}
+
+					@Override
+					public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+					}
+
+					@Override
+					public void surfaceDestroyed(SurfaceHolder holder) {
+					}
+				});
+				// Set up video output
+				final IVLCVout vout = mMediaPlayer.getVLCVout();
+				vout.setVideoView(mSurface);
+				//vout.setSubtitlesView(mSurfaceSubtitles);
+				vout.addCallback(this);
+				vout.attachViews();
+			}
+		}
+	}
+
+	@Override
+	public void onNewLayout(IVLCVout vout, int width, int height, int visibleWidth, int visibleHeight, int sarNum, int sarDen) {
+		if (width * height == 0)
+			return;
+
+		// store video size
+		mVideoWidth = width;
+		mVideoHeight = height;
+		setSize(mVideoWidth, mVideoHeight);
+	}
+
+	@Override
+	public void onSurfacesCreated(IVLCVout vout) {
+
+	}
+
+	@Override
+	public void onSurfacesDestroyed(IVLCVout vout) {
+		vout.removeCallback(this);
+		vout.detachViews();
+	}
+
+	@Override
+	public void eventHardwareAccelerationError() {
+		Log.e(TAG, "Error with hardware acceleration");
+		releasePlayer();
+		Toast.makeText(mCtx, "Error with hardware acceleration", Toast.LENGTH_LONG).show();
+
+	}
+
+	private void setSize(int width, int height) {
+		mVideoWidth = width;
+		mVideoHeight = height;
+		if (mVideoWidth * mVideoHeight <= 1)
+			return;
+
+		if(holder == null || mSurface == null)
+			return;
+
+		// get screen size
+		DisplayMetrics metrics = mCtx.getResources().getDisplayMetrics();
+		int w = metrics.widthPixels;
+		int h = metrics.heightPixels;
+
+		boolean isPortrait = mCtx.getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+		if (w > h && isPortrait || w < h && !isPortrait) {
+			int i = w;
+			w = h;
+			h = i;
+		}
+
+		float videoAR = (float) mVideoWidth / (float) mVideoHeight;
+		float screenAR = (float) w / (float) h;
+
+		if (screenAR < videoAR)
+			h = (int) (w / videoAR);
+		else
+			w = (int) (h * videoAR);
+
+		// force surface buffer size
+		holder.setFixedSize(mVideoWidth, mVideoHeight);
+
+		// set display size
+		ViewGroup.LayoutParams lp = mSurface.getLayoutParams();
+		lp.width = w;
+		lp.height = h;
+		mSurface.setLayoutParams(lp);
+		mSurface.invalidate();
+	}
+
+	private void releasePlayer() {
+		if (libvlc == null)
+			return;
+
+		final IVLCVout vout = mMediaPlayer.getVLCVout();
+		vout.removeCallback(this);
+		vout.detachViews();
+		holder = null;
+		libvlc.release();
+		libvlc = null;
+
+		mVideoWidth = 0;
+		mVideoHeight = 0;
+		mMediaPlayer.stop();
+		mMediaPlayer.release();
+		mMediaPlayer = null;
+	}
 }
