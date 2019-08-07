58c095aa0cc609e0d59c77de97b3fe2b78a86729
fredchen <fredchen@actions-micro.com>
2019-08-07 10:16:25 +0800

    Add VideoObj for mediastreaming 2.0

---
+++ src/main/java/com/actionsmicro/media/videoobj/AudioCodec.java
@@ -0,0 +1,25 @@
+package com.actionsmicro.media.videoobj;
+
+// POJO
+public class AudioCodec {
+    private String ext;
+    private String acodec;
+    private int abr;
+    private String src;
+
+    public String getExt() {
+        return ext;
+    }
+
+    public String getAcodec() {
+        return acodec;
+    }
+
+    public int getAbr() {
+        return abr;
+    }
+
+    public String getSrc() {
+        return src;
+    }
+}
