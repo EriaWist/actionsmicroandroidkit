4d3153ca66847321fdb8a34cdaf9bb559f93ee95
fredchen <fredchen@actions-micro.com>
2019-09-04 17:19:43 +0800

    Remove PlayListMediaNest

--- src/main/java/com/actionsmicro/media/webplaylist/youtubeplaylist/PlayListMediaFactory.java
+++ src/main/java/com/actionsmicro/media/webplaylist/youtubeplaylist/PlayListMediaFactory.java
@@ -7,31 +7,19 @@ import org.json.JSONObject;
 public class PlayListMediaFactory {
 
     public static final PlayListMedia createPlayListMedia(Context context, PlayListInfoItem item, PlayListMedia.PlayListMediaDelegate playListMediaDelegate) {
-        return createPlayListMedia(context, item, playListMediaDelegate, PlayListMedia.TYPE.TYPE_NEST);
+        return createPlayListMedia(context, item, playListMediaDelegate, PlayListMedia.TYPE.TYPE_FLAT);
     }
 
 
     public static final PlayListMedia createPlayListMedia(Context context, PlayListInfoItem item, PlayListMedia.PlayListMediaDelegate playListMediaDelegate, PlayListMedia.TYPE type) {
-        switch (type) {
-            case TYPE_NEST:
-                return new PlayListMediaNest(context, item, playListMediaDelegate);
-            case TYPE_FLAT:
-                return new PlayListMediaFlat(context, item, playListMediaDelegate);
-        }
-        return new PlayListMediaNest(context, item, playListMediaDelegate);
+        return new PlayListMediaFlat(context, item, playListMediaDelegate);
     }
 
     public static final PlayListMedia createPlayListMedia(Context context, JSONObject playListJson, PlayListMedia.PlayListMediaDelegate playListMediaDelegate, String parentTitle) {
-        return createPlayListMedia(context, playListJson, playListMediaDelegate, parentTitle, PlayListMedia.TYPE.TYPE_NEST);
+        return createPlayListMedia(context, playListJson, playListMediaDelegate, parentTitle, PlayListMedia.TYPE.TYPE_FLAT);
     }
 
     public static final PlayListMedia createPlayListMedia(Context context, JSONObject playListJson, PlayListMedia.PlayListMediaDelegate playListMediaDelegate, String parentTitle, PlayListMedia.TYPE type) {
-        switch (type) {
-            case TYPE_NEST:
-                return new PlayListMediaNest(context, playListJson, playListMediaDelegate, parentTitle);
-            case TYPE_FLAT:
-                return new PlayListMediaFlat(context, playListJson, playListMediaDelegate, parentTitle);
-        }
-        return new PlayListMediaNest(context, playListJson, playListMediaDelegate, parentTitle);
+        return new PlayListMediaFlat(context, playListJson, playListMediaDelegate, parentTitle);
     }
 }
