104c3b02f485c8bae30226b5886e197177997d02
fredchen <fredchen@actions-micro.com>
2020-11-03 15:00:06 +0800

    Deprecate ALGORITHM_AES_ECB

--- src/main/java/com/actionsmicro/utils/CipherUtil.java
+++ src/main/java/com/actionsmicro/utils/CipherUtil.java
@@ -2,8 +2,6 @@ package com.actionsmicro.utils;
 
 import android.util.Base64;
 
-import org.json.JSONObject;
-
 import java.io.PrintWriter;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
@@ -14,7 +12,6 @@ import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 public class CipherUtil {
-    public static final String ALGORITHM_AES_ECB = "AES/ECB/PKCS5Padding";
     public static final String ALGORITHM_AES_CBC = "AES/CBC/PKCS5Padding";
     private static final String TAG = "CipherUtil";
     private static final byte[] IV_BYTES = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00};
@@ -64,27 +61,10 @@ public class CipherUtil {
         }
     }
 
-    public static byte[] EncryptAES(byte[] key, byte[] text) {
-        return EncryptAES(key, text, ALGORITHM_AES_ECB);
-    }
-
     public static byte[] EncryptAES(byte[] key, byte[] text, String algorithm) {
-        if (algorithm.equals(ALGORITHM_AES_ECB)) {
-            return EncryptAESECB(key, text);
-        } else if (algorithm.equals(ALGORITHM_AES_CBC)) {
+        if (algorithm.equals(ALGORITHM_AES_CBC)) {
             return EncryptAESCBC(key, text);
         } else {
-            return null;
-        }
-    }
-
-    private static byte[] EncryptAESECB(byte[] key, byte[] text) {
-        try {
-            SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-            Cipher cipher = Cipher.getInstance(ALGORITHM_AES_ECB);
-            cipher.init(Cipher.ENCRYPT_MODE, skeySpec);
-            return cipher.doFinal(text);
-        } catch (Exception ex) {
             return null;
         }
     }
@@ -101,28 +81,10 @@ public class CipherUtil {
         }
     }
 
-    public static byte[] DecryptAES(byte[] key, byte[] text) {
-        return DecryptAES(key, text, ALGORITHM_AES_ECB);
-    }
-
     public static byte[] DecryptAES(byte[] key, byte[] text, String algorithm) {
-        if (algorithm.equals(ALGORITHM_AES_ECB)) {
-            return DecryptAESECB(key, text);
-        } else if (algorithm.equals(ALGORITHM_AES_CBC)) {
+        if (algorithm.equals(ALGORITHM_AES_CBC)) {
             return DecryptAESCBC(key, text);
         } else {
-            return null;
-        }
-    }
-
-    private static byte[] DecryptAESECB(byte[] key, byte[] text) {
-        try {
-            byte[] encryted_bytes = Base64.decode(text, Base64.DEFAULT);
-            SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-            Cipher cipher = Cipher.getInstance(ALGORITHM_AES_ECB);
-            cipher.init(Cipher.DECRYPT_MODE, skeySpec);
-            return cipher.doFinal(encryted_bytes);
-        } catch (Exception ex) {
             return null;
         }
     }
@@ -151,24 +113,6 @@ public class CipherUtil {
         return null;
     }
 
-    public static String EncryptAES(String content, String key) {
-        byte[] textByte;
-        try {
-            textByte = EncryptAES(key.getBytes("UTF-8"), content.getBytes("UTF-8"));
-            return Base64.encodeToString(textByte, Base64.DEFAULT);
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
-
-    public static String EncryptAES(JSONObject jsonObject, String key) {
-        if (null != jsonObject) {
-            return EncryptAES(jsonObject.toString(), key);
-        }
-        return null;
-    }
-
     public static String DecryptAES(String content, String key, String algorithm) {
         byte[] textByte;
         try {
@@ -178,9 +122,5 @@ public class CipherUtil {
             e.printStackTrace();
         }
         return null;
-    }
-
-    public static String DecryptAES(String content, String key) {
-        return DecryptAES(content, key, ALGORITHM_AES_ECB);
     }
 }
