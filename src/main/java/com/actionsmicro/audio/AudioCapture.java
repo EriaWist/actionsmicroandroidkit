c7bb17ed2d3ef0d11286d1ea7e4c0d17f32ba4f9
fredchen <fredchen@actions-micro.com>
2022-09-01 10:55:19 +0800

    Add mix pcm code for test

--- src/main/java/com/actionsmicro/audio/AudioCapture.java
+++ src/main/java/com/actionsmicro/audio/AudioCapture.java
@@ -36,6 +36,7 @@ public class AudioCapture {
     private static final int CHANNEL_COUNT = 2;
     private final int BUFFER_SIZE = CHANNEL_COUNT * 2 * 480;//AudioRecord.getMinBufferSize(SAMPLING_RATE, CHANNEL_IN_CONFIG, AUDIO_FORMAT);
     private final AudioRecord mAudioRecorder;
+    private final AudioRecord mAudioRecorder2;
     private final AudioDataCallback mAudioDataCallBack;
     private final boolean DUMP_AUDIO = false;
     private static final boolean DEBUG_LOG = false;
@@ -59,12 +60,20 @@ public class AudioCapture {
                         .build())
                 .setAudioPlaybackCaptureConfig(config)
                 .build();
+        mAudioRecorder2 = new AudioRecord.Builder()
+                .setAudioFormat(new AudioFormat.Builder()
+                        .setEncoding(AUDIO_FORMAT)
+                        .setSampleRate(SAMPLING_RATE)
+                        .setChannelMask(CHANNEL_IN_CONFIG)
+                        .build())
+                .setAudioSource(AUDIO_SOURCE)
+                .build();
     }
 
-    public void stopRecording() {
-        if (AudioRecord.STATE_INITIALIZED == mAudioRecorder.getState() && AudioRecord.RECORDSTATE_RECORDING == mAudioRecorder.getRecordingState()) {
-            synchronized (mAudioRecorder) {
-                mAudioRecorder.stop();
+    public void stopRecording(AudioRecord audioRecorder) {
+        if (AudioRecord.STATE_INITIALIZED == audioRecorder.getState() && AudioRecord.RECORDSTATE_RECORDING == audioRecorder.getRecordingState()) {
+            synchronized (audioRecorder) {
+                audioRecorder.stop();
             }
             Log.d(TAG, "Recording done…");
         }
@@ -73,9 +82,12 @@ public class AudioCapture {
     public void startRecording() {
         if (AudioRecord.STATE_INITIALIZED == mAudioRecorder.getState() && AudioRecord.RECORDSTATE_STOPPED == mAudioRecorder.getRecordingState()) {
             mAudioRecorder.startRecording();
+            mAudioRecorder2.startRecording();
             Log.d(TAG, "Start recording");
             new Thread() {
                 FileOutputStream testPCMFile = null;
+                FileOutputStream testPCMFile2 = null;
+                FileOutputStream testPCMFile3 = null;
                 FileOutputStream testAACFile = null;
                 FileOutputStream testRawAACFile = null;
 
@@ -84,8 +96,10 @@ public class AudioCapture {
                     if (DUMP_AUDIO) {
                         try {
                             testPCMFile = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/pcm.raw");
-                            testAACFile = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/adtsAAC.aac");
-                            testRawAACFile = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/rawAAC.aac");
+                            testPCMFile2 = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/pcm2.raw");
+                            testPCMFile3 = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/pcm3.raw");
+//                            testAACFile = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/adtsAAC.aac");
+//                            testRawAACFile = new FileOutputStream(Environment.getExternalStorageDirectory().getPath() + "/Download/rawAAC.aac");
                         } catch (FileNotFoundException e) {
                             // TODO Auto-generated catch block
                             e.printStackTrace();
@@ -94,6 +108,7 @@ public class AudioCapture {
 
                     try {
                         final byte[] pcmBuffer = new byte[BUFFER_SIZE];
+                        final byte[] pcmBuffer2 = new byte[BUFFER_SIZE];
                         final AudioTimestamp audioTimestamp = new AudioTimestamp();
                         Process.setThreadPriority(Process.THREAD_PRIORITY_URGENT_AUDIO);
 
@@ -105,22 +120,69 @@ public class AudioCapture {
                             private byte[] adtsHeader = new byte[7];
                             private int seq = 0;
                             private int needSize = BUFFER_SIZE;
+                            int pos1 = 0;
+                            int pos2 = 0;
 
+                            //   p1      p1n
+                            //       p2       p2n
+                            //   cur     com
                             @Override
                             public void onInputBufferAvailable(@NonNull IAacEldEncoder codec, int index) {
                                 ByteBuffer inputBuffer = codec.getInputBuffer(index);
                                 inputBuffer.clear();
-                                int ret = audioRecorder.read(pcmBuffer, 0, needSize, AudioRecord.READ_NON_BLOCKING);
+                                long start = System.currentTimeMillis();
+                                int ret = audioRecorder.read(pcmBuffer, pos1, BUFFER_SIZE - pos1, AudioRecord.READ_NON_BLOCKING);
+                                int ret2 = mAudioRecorder2.read(pcmBuffer2, pos2, BUFFER_SIZE - pos2, AudioRecord.READ_NON_BLOCKING);
+                                Log.d("dddd", "elapse " + (System.currentTimeMillis() - start) + " need " + needSize + " ret " + ret + " ret2 " + ret2);
+                                int len = 0;
                                 debugLog("onInputBufferAvailable audioRecorder.read: " + needSize);
-                                if (ret < 0) {
-                                    Log.e(TAG, "onInputBufferAvailable audioRecorder.read failed: " + ret);
-                                } else if (ret > 0) {
+                                if (ret < 0 || ret2 < 0) {
+                                    Log.e("dddd", "onInputBufferAvailable audioRecorder.read failed: " + ret);
+                                } else if (ret > 0 || ret2 >0) {
+
                                     audioRecorder.getTimestamp(audioTimestamp, AudioTimestamp.TIMEBASE_MONOTONIC);
                                     debugLog("onInputBufferAvailable AudioRecord timestamp position: " + audioTimestamp.framePosition + ", delta: " + (System.nanoTime() - audioTimestamp.nanoTime) / 1000000);
-                                    inputBuffer.put(pcmBuffer, 0, ret);
-                                    if (testPCMFile != null) {
+                                    int left = Math.min(pos1,pos2);
+                                    if (testPCMFile != null && ret >0) {
                                         try {
-                                            testPCMFile.write(pcmBuffer, 0, ret);
+                                            testPCMFile.write(pcmBuffer, pos1, ret);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    if (testPCMFile2 != null && ret2 >0) {
+                                        try {
+                                            testPCMFile2.write(pcmBuffer2, pos2, ret2);
+                                        } catch (IOException e) {
+                                            e.printStackTrace();
+                                        }
+                                    }
+                                    pos1 += ret;
+                                    pos2 += ret2;
+                                    int right = Math.min(pos1,pos2);
+                                    // mixed follow
+                                    // https://gist.github.com/mpuz/78e9e875df646698243affe1870dda58
+                                    len = right - left;
+                                    byte[] pcmBuffer3 = new byte[len];
+
+                                    for (int i = left; i < right; i++) {
+                                        float samplef1 = pcmBuffer[i] / 128.0f * 0.3f;      //     2^7=128
+                                        float samplef2 = pcmBuffer2[i] / 128.0f * 2.0f;
+                                        float mixed = samplef1 + samplef2;
+                                        // mixed *= 0.8;
+                                        // hard clipping
+                                        if (mixed > 1.0f) mixed = 1.0f;
+
+                                        if (mixed < -1.0f) mixed = -1.0f;
+
+                                        byte outputSample = (byte)(mixed * 128.0f);
+                                        pcmBuffer3[i-left] = outputSample;
+                                    }
+//                                    inputBuffer.put(pcmBuffer, 0, ret);
+                                    inputBuffer.put(pcmBuffer3, 0, len);
+                                    if (testPCMFile3 != null) {
+                                        try {
+                                            testPCMFile3.write(pcmBuffer3, 0, len);
                                         } catch (IOException e) {
                                             e.printStackTrace();
                                         }
@@ -128,14 +190,13 @@ public class AudioCapture {
                                 } else {
                                     debugLog("onInputBufferAvailable AudioRecord has nothing to read");
                                 }
-
                                 inputBuffer.rewind();
-                                needSize -= ret;
-                                if (needSize == 0) {
-                                    codec.queueInputBuffer(index, 0, ret, System.nanoTime() / 1000, 0);
-                                    needSize = BUFFER_SIZE;
+                                if (pos1 == BUFFER_SIZE && pos2 == BUFFER_SIZE) {
+                                    codec.queueInputBuffer(index, 0, len, System.nanoTime() / 1000, 0);
+                                    pos1 = 0;
+                                    pos2 = 0;
                                 } else {
-                                    codec.queueInputBuffer(index, 0, ret, System.nanoTime() / 1000, MediaCodec.BUFFER_FLAG_PARTIAL_FRAME);
+                                    codec.queueInputBuffer(index, 0, len, System.nanoTime() / 1000, MediaCodec.BUFFER_FLAG_PARTIAL_FRAME);
                                 }
                             }
 
@@ -197,11 +258,16 @@ public class AudioCapture {
                             }
                         });
                         audioRecorder.startRecording();
+                        mAudioRecorder2.startRecording();
+                        Log.e("eeee","start");
                         codec.start();
                         Looper.loop();
                         codec.stop();
+                        Log.e("eeee","stop");
                         audioRecorder.stop();
                         audioRecorder.release();
+                        mAudioRecorder2.stop();
+                        mAudioRecorder2.release();
                         if (testPCMFile != null) {
                             testPCMFile.close();
                         }
@@ -228,7 +294,8 @@ public class AudioCapture {
     }
 
     public void release() {
-        stopRecording();
+        stopRecording(mAudioRecorder);
+        stopRecording(mAudioRecorder2);
         if (AudioRecord.STATE_INITIALIZED == mAudioRecorder.getState()) {
             mAudioRecorder.release();
             Log.d(TAG, "Release recorder");
