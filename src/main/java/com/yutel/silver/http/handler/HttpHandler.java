1829ff7b5ee1abba8dd9313fe065f4ae9a57f023
fredchen <fredchen@actions-micro.com>
2023-09-25 14:58:26 +0800

    Remove EZScreenServer/EZScreenHelper

--- src/main/java/com/yutel/silver/http/handler/HttpHandler.java
+++
@@ -1,8 +0,0 @@
-package com.yutel.silver.http.handler;
-
-import com.yutel.silver.exception.AirplayException;
-import com.yutel.silver.http.HttpWrap;
-
-public interface HttpHandler {
-	public void handle(HttpWrap hw) throws AirplayException;
-}
